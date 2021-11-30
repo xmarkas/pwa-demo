0844c7eb3a55f5378606ed156579120bb1732bbd
Mark <xmarkas@hotmail.com>
2021-11-30 00:56:24 -0800

    fix router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense} from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -11,20 +11,20 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/about"><About /></Route>
-        <Route path="/"><Home /></Route>
-      </Routes>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </Suspense>
     </Router>
   );
